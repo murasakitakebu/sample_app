fb72b83e8cab78a8c58345f369b983ccf99cf8fe
murasakitakebu <dynamic7lgnd@gmail.com>
2018-04-09 16:55:20 +0900

    Finish layout and routes

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2  #root_pathへは2通りの生き方があるかどうか
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+end
